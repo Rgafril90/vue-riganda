06cdaf74d8937e91601a974ff40c2234740ae60d
Rgafril90 <93838735+Rgafril90@users.noreply.github.com>
2021-11-24 10:05:32 +0700

    router

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+//
+// import HelloWorld from import('@/component/HelloWorld')
+
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: () => import('@/components/HelloWorld')
+    },
+
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('@/views/login/main')
+    },
+
+    {
+        path: '/register',
+        name: 'register',
+        component: () => import('@/views/register/main')
+    }
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router;
