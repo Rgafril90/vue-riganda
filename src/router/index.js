8aa6fe82beb260cb7645a78d580fe700e77bf732
Rgafril90 <93838735+Rgafril90@users.noreply.github.com>
2021-12-06 08:08:05 +0700

    Membuat form create users

--- src/router/index.js
+++ src/router/index.js
@@ -3,46 +3,59 @@ import { createRouter, createWebHashHistory } from "vue-router";
 //
 // import HelloWorld from import('@/component/HelloWorld')
 
-
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: () => import('@/components/HelloWorld')
-    },
+  {
+    path: "/",
+    name: "home",
+    component: () => import("@/components/HelloWorld"),
+  },
 
-    {
-        path: '/login',
-        name: 'login',
-        component: () => import('@/views/login/main')
-    },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/login/main"),
+  },
 
-    {
-        path: '/register',
-        name: 'register',
-        component: () => import('@/views/register/main')
-    },
-    {
-        path: '/Dashboard',
-        name: 'Dashboard',
-        component: () => import('@/views/Dashboard/main')
-    },
-    {
-        path: '/Profil',
-        name: 'Profil',
-        component: () => import('@/views/Profil/main')
-    },
-    {
-        path: '/About',
-        name: 'About',
-        component: () => import('@/views/About/main')
-    }
-    
+  {
+    path: "/register",
+    name: "register",
+    component: () => import("@/views/register/main"),
+  },
+  {
+    path: "/Dashboard",
+    name: "Dashboard",
+    component: () => import("@/views/Dashboard/main"),
+  },
+  {
+    path: "/Profil",
+    name: "Profil",
+    component: () => import("@/views/Profil/main"),
+  },
+  {
+    path: "/About",
+    name: "About",
+    component: () => import("@/views/About/main"),
+  },
+  {
+    path: "/users",
+    name: "users",
+    component: () => import("@/views/users/main"),
+  },
+  {
+    path: "/users/:id/:username?",
+    name: "detailuser",
+    component: () => import("@/views/users/detailUser"),
+  },
+  {
+    path: "/create-user",
+    name: "createuser",
+    component: () => import("@/views/users/FormCreate"),
+  },
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes
-})
+  history: createWebHashHistory(),
+  routes,
+});
 
 export default router;
